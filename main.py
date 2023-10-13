4440b413940eede141da864e0d70bfc890828e8c
777sleepy777 <138101673+777sleepy777@users.noreply.github.com>
2023-10-13 20:23:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,135 +1,178 @@
-from collections import UserDict
-def input_error(func):
-    def inner(*args):
+from datetime import datetime as dt, timedelta
+from collections import UserList
+import pickle
+from info import *
+import os
+
+
+class AddressBook(UserList):
+    def __init__(self):
+        self.data = []
+        self.counter = -1
+
+    def __str__(self):
+        result = []
+        for account in self.data:
+            if account['birthday']:
+                birth = account['birthday'].strftime("%d/%m/%Y")
+            else:
+                birth = ''
+            if account['phones']:
+                new_value = []
+                for phone in account['phones']:
+                    print(phone)
+                    if phone:
+                        new_value.append(phone)
+                phone = ', '.join(new_value)
+            else:
+                phone = ''
+            result.append(
+                "_" * 50 + "\n" + f"Name: {account['name']} \nPhones: {phone} \nBirthday: {birth} \nEmail: {account['email']} \nStatus: {account['status']} \nNote: {account['note']}\n" + "_" * 50 + '\n')
+        return '\n'.join(result)
+
+    def __next__(self):
+        phones = []
+        self.counter += 1
+        if self.data[self.counter]['birthday']:
+            birth = self.data[self.counter]['birthday'].strftime("%d/%m/%Y")
+        if self.counter == len(self.data):
+            self.counter = -1
+            raise StopIteration
+        for number in self.data[self.counter]['phones']:
+            if number:
+                phones.append(number)
+        result = "_" * 50 + "\n" + f"Name: {self.data[self.counter]['name']} \nPhones: {', '.join(phones)} \nBirthday: {birth} \nEmail: {self.data[self.counter]['email']} \nStatus: {self.data[self.counter]['status']} \nNote: {self.data[self.counter]['note']}\n" + "_" * 50
+        return result
+
+    def __iter__(self):
+        return self
+
+    def __setitem__(self, index, record):
+        self.data[index] = {'name': record.name,
+                            'phones': record.phones,
+                            'birthday': record.birthday}
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def log(self, action):
+        current_time = dt.strftime(dt.now(), '%H:%M:%S')
+        message = f'[{current_time}] {action}'
+        with open('logs.txt', 'a') as file:
+            file.write(f'{message}\n')
+
+    def add(self, record):
+        account = {'name': record.name,
+                   'phones': record.phones,
+                   'birthday': record.birthday,
+                   'email': record.email,
+                   'status': record.status,
+                   'note': record.note}
+        self.data.append(account)
+        self.log(f"Contact {record.name} has been added.")
+
+    def save(self, file_name):
+        with open(file_name + '.bin', 'wb') as file:
+            pickle.dump(self.data, file)
+        self.log("Addressbook has been saved!")
+
+    def load(self, file_name):
+        emptyness = os.stat(file_name + '.bin')
+        if emptyness.st_size != 0:
+            with open(file_name + '.bin', 'rb') as file:
+                self.data = pickle.load(file)
+            self.log("Addressbook has been loaded!")
+        else:
+            self.log('Adressbook has been created!')
+        return self.data
+
+    def search(self, pattern, category):
+        result = []
+        category_new = category.strip().lower().replace(' ', '')
+        pattern_new = pattern.strip().lower().replace(' ', '')
+
+        for account in self.data:
+            if category_new == 'phones':
+
+                for phone in account['phones']:
+
+                    if phone.lower().startswith(pattern_new):
+                        result.append(account)
+            elif account[category_new].lower().replace(' ', '') == pattern_new:
+                result.append(account)
+        if not result:
+            print('There is no such contact in address book!')
+        return result
+
+    def edit(self, contact_name, parameter, new_value):
+        names = []
         try:
-            result = func(*args)
-            return result
-        except KeyError:
-            return "KeyError. This name is not in phone-book"
+            for account in self.data:
+                names.append(account['name'])
+                if account['name'] == contact_name:
+                    if parameter == 'birthday':
+                        new_value = Birthday(new_value).value
+                    elif parameter == 'email':
+                        new_value = Email(new_value).value
+                    elif parameter == 'status':
+                        new_value = Status(new_value).value
+                    elif parameter == 'phones':
+                        new_contact = new_value.split(' ')
+                        new_value = []
+                        for number in new_contact:
+                             new_value.append(Phone(number).value)
+                    if parameter in account.keys():
+                        account[parameter] = new_value
+                    else:
+                        raise ValueError
+            if contact_name not in names:
+                raise NameError
         except ValueError:
-            return "ValueError. Phone number must be from 10 digit"
-        except TypeError:
-            return "TypeError. Unknown command"
-        except IndexError:
-            return "IndexError. Give me name and phone please"
-    return inner
-#Базовий клас для полів запису. 
-#Буде батьківським для всіх полів, у ньому реалізується логіка загальна для всіх полів
-class Field: 
-    def __init__(self, value):
-        self.value = value
+            print('Incorrect parameter! Please provide correct parameter')
+        except NameError:
+            print('There is no such contact in address book!')
+        else:
+            self.log(f"Contact {contact_name} has been edited!")
+            return True
+        return False
 
-    def __str__(self):
-        return str(self.value)
+    def remove(self, pattern):
+        flag = False
+        for account in self.data:
+            if account['name'] == pattern:
+                self.data.remove(account)
+                self.log(f"Contact {account['name']} has been removed!")
+                flag = True
+            '''if pattern in account['phones']:
+                        account['phones'].remove(pattern)
+                        self.log.log(f"Phone number of {account['name']} has been removed!")'''
+        return flag
 
-    
-#Клас для зберігання імені контакту. Обов'язкове поле.
-class Name(Field):
-    pass
-    # реалізація класу
+    def __get_current_week(self):
+        now = dt.now()
+        current_weekday = now.weekday()
+        if current_weekday < 5:
+            week_start = now - timedelta(days=2 + current_weekday)
+        else:
+            week_start = now - timedelta(days=current_weekday - 5)
+        return [week_start.date(), week_start.date() + timedelta(days=7)]
 
-class Phone(Field):
-    def __init__(self, value):
-        self.value = value
-        if len(self.value) != 10 or not self.value.isdigit():
-            raise ValueError("Number is not valid")
-    
-    def __str__(self):
-        return f"{self.value}"
-   
- 
-class Record():
-    def __init__(self, name, phone=None):
-        self.name = Name(value=name)
-        self.phones = []
-        if phone:
-            self.add_phone_number(value = phone)
+    def congratulate(self):
+        result = []
+        WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        current_year = dt.now().year
+        congratulate = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+        for account in self.data:
+            if account['birthday']:
+                new_birthday = account['birthday'].replace(year=current_year)
+                birthday_weekday = new_birthday.weekday()
+                if self.__get_current_week()[0] <= new_birthday.date() < self.__get_current_week()[1]:
+                    if birthday_weekday < 5:
+                        congratulate[WEEKDAYS[birthday_weekday]].append(account['name'])
+                    else:
+                        congratulate['Monday'].append(account['name'])
+        for key, value in congratulate.items():
+            if len(value):
+                result.append(f"{key}: {' '.join(value)}")
+        return '_' * 50 + '\n' + '\n'.join(result) + '\n' + '_' * 50
 
-
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
-   
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-
-    def edit_phone(self, phone_old, phone_new):
-        num = None
-        for i in self.phones:
-            if i.value == phone_old:
-                num = phone_old
-                i.value = phone_new
-
-        if num is None:
-        #    raise ValueError
-        #if phone_old in self.phones:
-        #   phone_old.value = phone_new 
-        #else:
-            raise ValueError
-
-
-
-    def remove_phone(self, phone):
-         for i in self.phones:
-            if i.value == phone:
-                self.phones.remove(i)
-
-    def find_phone(self, phone):
-        for i in self.phones:
-            if i.value == phone:
-                return i
-   
-        
-class AddressBook(UserDict):
-    def add_record(self, record: Record):
-        self.data[record.name.value] = record
-    
-    def find(self, name: Name):
-         for i in self.data:
-            if i == name:
-                return self.data[i]
-
-    
-    def delete(self, name: Name):
-        try:
-            self.data.pop(name)
-        except:
-            KeyError
-
-        
-
-def main():
-  
-    book = AddressBook()
-
-    # Створення запису для John
-    john_record = Record("John")
-    john_record.add_phone("1234567890")
-    john_record.add_phone("5555555555")
-
-    # Додавання запису John до адресної книги
-    book.add_record(john_record)
-
-    # Створення та додавання нового запису для Jane
-    jane_record = Record("Jane")
-    jane_record.add_phone("9876543210")
-    book.add_record(jane_record)
-
-    # Виведення всіх записів у книзі
-    
-
-    # Знаходження та редагування телефону для John
-    john = book.find("John")
-    john.edit_phone("1234567890", "1112223333")
-
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-    # Пошук конкретного телефону у записі John
-    found_phone = john.find_phone("5555555555")
-    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-
-    # Видалення запису Jane
-    book.delete("Jane")
-
-    
-if __name__ == '__main__':
-    main()
